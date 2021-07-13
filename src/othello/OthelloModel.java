89cacf9b84ecbbcc1a28d041f6e618c57bd45cf9
Tyson Moyes <tysonjfmoyes@gmail.com>
2021-07-13 10:51:31 -0400

    Added the initial OthelloModel file, more work to be done on this later

---
+++ src/othello/OthelloModel.java
@@ -0,0 +1,27 @@
+package othello;
+
+public class OthelloModel {
+    public int getSquare(int row, int col) {
+        return 0;
+    }
+
+    public void prepareBoard(int mode) {
+
+    }
+
+    public boolean canMove(int row, int col, int player) {
+        return false;
+    }
+
+    public int tryMove(int row, int col, int player) {
+        return 0;
+    }
+
+    public boolean moveTest(int player) {
+        return true;
+    }
+
+    public int chipCount(int player) {
+        return 0;
+    }
+}
