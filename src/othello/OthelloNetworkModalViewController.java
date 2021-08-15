4508da42d3435e78cf79f89e0415b216038bec84
Tyson Moyes <tysonjfmoyes@gmail.com>
2021-08-15 13:32:30 -0400

    Updated network UI for A22 requirements

--- src/othello/OthelloNetworkModalViewController.java
+++ src/othello/OthelloNetworkModalViewController.java
@@ -15,6 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import java.util.regex.Pattern;
 
 import com.sun.glass.events.KeyEvent;
 
@@ -42,13 +43,11 @@ public class OthelloNetworkModalViewController extends JDialog {
      * Instance of the ComboBox you need to build. You'll want to change this
      * comment.
      */
-    // NOTE: In the case of this and other UI elements, you may rename them as you
-    // see fit.
-    // Just be careful that you catch every instance of it if you do.
-    // I was getting the following warning about unparameterized JComboBoxes:
-    // "JComboBox is a raw type. References to generic type JComboBox<E> should be
-    // parameterized"
-    // I fixed by adding the <String> parameter
+    /*
+     * NOTE: I was getting the following warning about unparameterized JComboBoxes:
+     * "JComboBox is a raw type. References to generic type JComboBox<E> should be
+     * parameterized" I fixed by adding the <String> parameter
+     */
     JComboBox<String> portInput;
 
     /** Textfield for entering the FQDN/IP address */
@@ -88,91 +87,41 @@ public class OthelloNetworkModalViewController extends JDialog {
         GridBagConstraints c = new GridBagConstraints();
         c.ipadx = 5;
         c.ipady = 5;
+        c.fill = GridBagConstraints.HORIZONTAL;
 
+        // Start with the gridwidth 1 items
+        c.gridwidth = 1;
+        // ADDRESS LABEL
         JLabel addressLabel = new JLabel("Address:");
         addressLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         addressLabel.setDisplayedMnemonic(KeyEvent.VK_A);
         c.anchor = GridBagConstraints.LINE_END;
-        c.gridwidth = 1;
         c.gridx = 0;
         c.gridy = 0;
         c.insets = new Insets(10, 5, 0, 5);
         pane.add(addressLabel, c);
 
-        addressInput = new JTextField(20);
-        addressLabel.setLabelFor(addressInput);
-        c.anchor = GridBagConstraints.LINE_START;
-        c.gridwidth = 3;
-        c.gridx = 1;
-        c.gridy = 0;
-        c.insets = new Insets(10, 0, 0, 5);
-        pane.add(addressInput, c);
-
-        addressError = new JLabel();
-        addressError.setForeground(Color.RED);
-        c.gridx = 1;
-        c.gridy = 1;
-        c.weightx = 1;
-        c.insets = new Insets(0, 0, 5, 0);
-        pane.add(addressError, c);
-
+        // PORT LABEL
         JLabel portLabel = new JLabel("Port:");
         portLabel.setDisplayedMnemonic(KeyEvent.VK_P);
         portLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         c.anchor = GridBagConstraints.LINE_END;
-        c.gridwidth = 1;
         c.gridx = 0;
         c.gridy = 2;
         c.insets = new Insets(5, 5, 0, 5);
         pane.add(portLabel, c);
 
-        String portOptions[] = { "", "32300", "42300", "52300" };
-        portInput = new JComboBox<String>(portOptions);
-        portLabel.setLabelFor(portInput);
-        portInput.setSelectedIndex(0);
-        portInput.setEditable(true);
-        c.anchor = GridBagConstraints.LINE_START;
-        c.gridwidth = 2;
-        c.gridx = 1;
-        c.gridy = 2;
-        c.insets = new Insets(5, 0, 0, 5);
-        pane.add(portInput, c);
-
-        portError = new JLabel();
-        portError.setForeground(Color.RED);
-        c.gridwidth = 3;
-        c.gridx = 1;
-        c.gridy = 3;
-        c.insets = new Insets(0, 0, 5, 0);
-        pane.add(portError, c);
-
+        // NAME LABEL
         JLabel nameLabel = new JLabel("Name:");
         nameLabel.setDisplayedMnemonic(KeyEvent.VK_N);
         nameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         c.anchor = GridBagConstraints.LINE_END;
-        c.gridwidth = 1;
         c.gridx = 0;
         c.gridy = 4;
         c.insets = new Insets(5, 5, 0, 5);
         pane.add(nameLabel, c);
 
-        nameInput = new JTextField(20);
-        nameLabel.setLabelFor(nameInput);
-        c.anchor = GridBagConstraints.LINE_START;
-        c.gridwidth = 3;
-        c.gridx = 1;
-        c.gridy = 4;
-        c.insets = new Insets(5, 0, 0, 5);
-        pane.add(nameInput, c);
-
-        nameError = new JLabel();
-        nameError.setForeground(Color.RED);
-        c.gridwidth = 1;
-        c.gridx = 1;
-        c.gridy = 5;
-        c.insets = new Insets(0, 0, 5, 0);
-        pane.add(nameError, c);
-
+        // CONNECT AND CANCEL BUTTONS
         JButton connectButton = new JButton("Connect");
         connectButton.setActionCommand("C");
         connectButton.addActionListener(handler);
@@ -180,15 +129,74 @@ public class OthelloNetworkModalViewController extends JDialog {
         cancelButton.setActionCommand("X");
         cancelButton.addActionListener(handler);
 
-        c.anchor = GridBagConstraints.CENTER;
-        c.gridwidth = 1;
-        c.gridx = 1;
+        c.anchor = GridBagConstraints.LINE_END;
+        c.fill = GridBagConstraints.NONE;
+        c.gridx = 2;
         c.gridy = 6;
         pane.add(connectButton, c);
 
         c.gridx = 3;
         c.gridy = 6;
         pane.add(cancelButton, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        // PORT OPTIONS (gridwidth 2)
+        c.gridwidth = 2;
+        String portOptions[] = { "", "32300", "42300", "52300" };
+        portInput = new JComboBox<String>(portOptions);
+        portLabel.setLabelFor(portInput);
+        portInput.setSelectedIndex(0);
+        portInput.setEditable(true);
+        c.anchor = GridBagConstraints.LINE_START;
+        c.gridx = 1;
+        c.gridy = 2;
+        c.insets = new Insets(5, 0, 0, 5);
+        pane.add(portInput, c);
+
+        // Now the GridWidth 3 items
+        c.gridwidth = 3;
+        // ADDRESS INPUT
+        addressInput = new JTextField(20);
+        addressLabel.setLabelFor(addressInput);
+        c.anchor = GridBagConstraints.LINE_START;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.insets = new Insets(5, 0, 0, 5);
+        pane.add(addressInput, c);
+
+        // ADDRESS ERROR
+        addressError = new JLabel(" ");
+        addressError.setForeground(Color.RED);
+        c.gridx = 1;
+        c.gridy = 1;
+        c.weightx = 1;
+        c.insets = new Insets(0, 0, 5, 0);
+        pane.add(addressError, c);
+
+        // PORT ERROR
+        portError = new JLabel(" ");
+        portError.setForeground(Color.RED);
+        c.gridx = 1;
+        c.gridy = 3;
+        c.insets = new Insets(0, 0, 5, 0);
+        pane.add(portError, c);
+
+        // NAME INPUT
+        nameInput = new JTextField(20);
+        nameLabel.setLabelFor(nameInput);
+        c.anchor = GridBagConstraints.LINE_START;
+        c.gridx = 1;
+        c.gridy = 4;
+        c.insets = new Insets(5, 0, 0, 5);
+        pane.add(nameInput, c);
+
+        // NAME ERROR
+        nameError = new JLabel(" ");
+        nameError.setForeground(Color.RED);
+        c.gridx = 1;
+        c.gridy = 5;
+        c.insets = new Insets(0, 0, 5, 0);
+        pane.add(nameError, c);
 
         networkPanel.add(pane);
         // And when you're done, of course you'll pack it.
@@ -251,19 +259,15 @@ public class OthelloNetworkModalViewController extends JDialog {
      */
 
     public void hideModal() {
-        // Add any code that you may want to do after the user input has been processed
-        // and you're figuratively closing up the shop.
-        // Once this method is done, control will return to the code that set this
-        // dialog
-        // to be visible.
-
-        // As a suggestion, you may want to reset error messages
-        // and potentially clear out the input for part 2, especially if cancel was
-        // pressed.
-        // It's up to you, of course.
-
+        addressError.setText("");
+        portError.setText("");
+        nameError.setText("");
+        if (!hasConnected) {
+            addressInput.setText("");
+            portInput.setSelectedIndex(0);
+            nameInput.setText("");
+        }
         setVisible(false);
-
     }
 
     /**
@@ -293,20 +297,37 @@ public class OthelloNetworkModalViewController extends JDialog {
             // User selects "Connect": I set "C" to be my Connect button's "action command".
             if ("C".equals(s)) // Connect option.
             {
-                hasConnected = true;
-
+                Boolean isValid = true;
+                String addressValue = addressInput.getText();
+                String portValue = portInput.getSelectedItem().toString();
+                String nameValue = nameInput.getText();
                 // Do error checking and report error messages as required
                 // in part 2.
+                if (addressValue.isEmpty()) {
+                    addressError.setText("The address must not be blank.");
+                    isValid = false;
+                }
+                if (Pattern.matches("[0-9]+", portValue) == false) {
+                    portError.setText("The port must be an integer.");
+                    isValid = false;
+                }
+                if (nameValue.length() < 3) {
+                    nameError.setText("Name too short.");
+                    isValid = false;
+                }
+
+                if (isValid) {
+                    // Valid connection
+                    hasConnected = true;
+                    hideModal();
+                }
+
             }
             // I set "X" to be my Cancel button's "action command":
             else {
                 hasConnected = false;
+                hideModal();
             }
-
-            // In either case, for NOW, we're done with the modal, so let's call our
-            // cleanup method and return control to the main UI
-
-            hideModal();
         }
     }
 }
